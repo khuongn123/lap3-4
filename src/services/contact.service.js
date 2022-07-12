efb8c4240fe7d49ad3463f704ca89ed94fce01f2
khuongn123 <107014206+khuongn123@users.noreply.github.com>
2022-07-12 16:53:54 +0700

    Hien thi danh sach cac contact lay ve tu server

--- src/services/contact.service.js
+++ src/services/contact.service.js
@@ -1,7 +1,7 @@
 import createApiClient from "./api.service";
 
 class ContactService {
-    constructor(baseUrl = "/api/contacts") {
+    constructor(baseUrl = "/api/contacts"){
         this.api = createApiClient(baseUrl);
     }
     async getAll() {
