fda6035400b525576ed544331afd616cbbf7f093
khuongn123 <107014206+khuongn123@users.noreply.github.com>
2022-07-12 16:49:42 +0700

    Hien thi danh sach cac contact lay ve tu server

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,7 +3,7 @@
         <a href="/" class="navbar-brand">Ứng dụng Quản lý danh bạ</a>
         <div class="mr-auto navbar-nav">
             <li class="nav-item">
-                <router-link :to="{ name: 'contactbook' }" class="nav-link">
+                <router-link :to="{ name: 'contactbook' }"class="nav-link">
                     Danh bạ
                     <i class="fas fa-address-book"></i>
                 </router-link>
