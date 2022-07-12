6444f1d66a1654adb1cdcb3c1ad8bb13fea1ecb1
khuongn123 <107014206+khuongn123@users.noreply.github.com>
2022-07-12 16:55:08 +0700

    Cai dat trang loi 404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="page">
         <p>
-            Oops, không thể tìm thấy trang.
+            Oops,không thể tìm thấy trang.
             <router-link to="/">Trở về trang chủ.</router-link>
         </p>
     </div>
