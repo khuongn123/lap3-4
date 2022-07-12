40ae846f7f473c2c3076940b3e90d1458d053cdc
khuongn123 <107014206+khuongn123@users.noreply.github.com>
2022-07-12 16:56:18 +0700

    Tao trang cao nhat lien he

--- src/views/ContactBook.vue
+++ src/views/ContactBook.vue
@@ -40,9 +40,9 @@
                     params: { id: activeContact._id },
                     }"
                 >
-                    <span class="mt-2 badge badge-warning">
-                        <i class="fas fa-edit"></i> Hiệu chỉnh</span
-                    >
+                <span class="mt-2 badge badge-warning">
+                    <i class="fas fa-edit"></i> Hiệu chỉnh</span
+                >
                 </router-link>
             </div>
         </div>
